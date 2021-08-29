35d6cc6e8a80d56a1fe29503960b2c7b99e9f27a
Jeff Huang <jeff@digisalad.cool>
2021-08-29 17:07:36 +0800

    add lang settng

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,28 @@
-import theme from '@nuxt/content-theme-docs'
+import theme from '@nuxt/content-theme-docs';
 
 export default theme({
-  docs: {
-    primaryColor: '#E24F55'
-  }
+	docs: {
+		primaryColor: '#E24F55'
+	},
+	loading: { color: '#00CD81' },
+	i18n: {
+		locales: () => [{
+			code: 'zh-TW',
+			iso: 'zh-TW',
+			file: 'zh-TW.js',
+			name: '中文'
+		}, {
+			code: 'en',
+			iso: 'en-US',
+			file: 'en-US.js',
+			name: 'English'
+		}],
+		defaultLocale: 'zh-TW',
+		vueI18n: {
+			fallbackLocale: 'zh-TW',
+			silentTranslationWarn: true,
+		}
+	},
+	buildModules: [
+	]
 })
